70bae701b1c1d15f6ec21164464994b3c6d3b4f6
Noa-GH <fordnt17@gmail.com>
2026-05-13 19:59:21 -0400

    Refactor Profile component: remove TypeScript version and update props handling for user actions

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,20 +2,25 @@ import "./Profile.css";
 import SideBar from "./SideBar/SideBar.jsx";
 import ClothesSection from "./ClothesSection/ClothesSection.jsx";
 
-function Profile({ clothingItems, onCardClick, handleAddClick, currentUser }) {
+function Profile({
+  clothingItems,
+  onCardClick,
+  handleAddClick,
+  onEditProfile,
+  onSignOut,
+  onCardLike,
+}) {
   return (
     <div className="profile">
-      <SideBar
-        className="profile__sidebar"
-        currentUser={currentUser}
-      />
+      <SideBar onEditProfile={onEditProfile} onSignOut={onSignOut} />
       <ClothesSection
         className="clothes-section"
         clothingItems={clothingItems}
         onCardClick={onCardClick}
         handleAddClick={handleAddClick}
+        onCardLike={onCardLike}
       />
-    </div >
+    </div>
   );
 }
 
